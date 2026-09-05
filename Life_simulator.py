8efe05f772b31c8b7a78f00a7765afbcf83e7e0e
Himal <Hyaridev7@gmail>
2026-09-05 11:51:57 +0545

    first commit

---
+++ Life_simulator.py
@@ -0,0 +1,23 @@
+print("==== LIFE SIMULATOR ====")
+print("Welcome to your new life. ")
+print("Create your character: ")
+
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+
+print(f"Welcome, {name}! Let's begin your new life.")
+
+health = 70
+happiness = 70
+money = 80
+iq = 90
+social = 50
+
+print("Your starting stats: ")
+print(f"Health: {health}")
+print(f"happiness: {happiness}")
+print(f"money: {money}")
+print(f"intelligence: {iq}")
+print(f"social: {social}")
+
+
